720fcddc3f181e8a173f79bacfed4ce47ed69640
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-12 22:51:50 +0200

    Initial Commit

---
+++ Shared/Carousel_CustomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel_CustomApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 12/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Carousel_CustomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
